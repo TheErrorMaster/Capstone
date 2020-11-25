509c648458fd7dfff61ddb5e1025ad4ac0b8ca4a
Riz <ReederLoveland@gmail.com>
2020-11-25 11:42:31 -0800

    Gameplay is functional for a single player
    player may choose and unchoose any dice. they may reroll or end turn. an unsuccessful reroll zeros out points and should end turn. Players may also keep rolling for points when they have scored on all dice.

--- MainFile/app.py
+++ MainFile/app.py
@@ -9,7 +9,6 @@ root.title('10,000 Dice')
 root.geometry("400x400")
 root.config(bg="green")
 root.resizable(False, False) #Disallow players from resizing the window.
-stringme=StringVar()
 dice_btns = []
 curr_pts = 0
 points=IntVar() # the current amount of points
@@ -23,10 +22,10 @@ total=IntVar() #the total amount of points
 #'\u2684' = dice-five
 #'\u2685' = dice-six
 dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
-
 dice_rolled=[]
 dice_kept=[]
 curr_dice=[]
+dice_count=[0] * 6
 
 for i in range(0, 6):
     dice_rolled.append(StringVar())
@@ -45,73 +44,94 @@ def randomGen():
     for i in range(0, 6):
         dice_rolled[i].set(random.choice(dice))
 
-def keep_dice(args):
+    dice_count = [0] * 6
+    for i in range(0, 6):
+        if (dice_rolled[i].get() == dice[0]):
+            dice_count[0] += 1
+        if (dice_rolled[i].get() == dice[1]):
+            dice_count[1] += 1
+        if (dice_rolled[i].get() == dice[2]):
+            dice_count[2] += 1
+        if (dice_rolled[i].get() == dice[3]):
+            dice_count[3] += 1
+        if (dice_rolled[i].get() == dice[4]):
+            dice_count[4] += 1
+        if (dice_rolled[i].get() == dice[5]):
+            dice_count[5] += 1
+
+def keep_dice(args):#counts dice overall to test for 3s
     current_score = calc_points()
     button = dice_btns[args]
     #add dice to score if unadded
     if dice_kept[args].get() == '' and curr_dice[args].get() == '':
         curr_dice[args].set(dice_rolled[args].get())
-        #only add dice if points are available
-        if current_score == calc_points():
-            curr_dice[args].set('')
-        else:
-            button.config(relief=SUNKEN)
+        button.config(relief=SUNKEN)
     #take dice from score if already added
-    elif dice_kept == '':
+    elif dice_kept[args].get() == '':
         curr_dice[args].set('')
         button.config(relief=RAISED)
     curr_pts = calc_points()
     points.set(points.get()-current_score+curr_pts)
-
-    for i in range(0, 6):
-        if dice_kept[i].get() == '':
-            return
     #reset dice and continue to roll
 
+def is_triple_position(arg):
+    face = 0
+    if (dice_rolled[arg].get() == dice[0]):
+        face = 0
+    if (dice_rolled[arg].get() == dice[1]):
+        face = 1
+    if (dice_rolled[arg].get() == dice[2]):
+        face = 2
+    if (dice_rolled[arg].get() == dice[3]):
+        face = 3
+    if (dice_rolled[arg].get() == dice[4]):
+        face = 4
+    if (dice_rolled[arg].get() == dice[5]):
+        face = 5
+    return dice_count[face] > 2
         
-def calc_points():
+def calc_points():#counts dice for current for points
     temp_score = 0
-    dice_count = [0] * 6
+    curr_dice_count = [0] * 6
     for i in range(0, 6):
         if (curr_dice[i].get() == dice[0]):
-            dice_count[0] += 1
+            curr_dice_count[0] += 1
         if (curr_dice[i].get() == dice[1]):
-            dice_count[1] += 1
+            curr_dice_count[1] += 1
         if (curr_dice[i].get() == dice[2]):
-            dice_count[2] += 1
+            curr_dice_count[2] += 1
         if (curr_dice[i].get() == dice[3]):
-            dice_count[3] += 1
+            curr_dice_count[3] += 1
         if (curr_dice[i].get() == dice[4]):
-            dice_count[4] += 1
+            curr_dice_count[4] += 1
         if (curr_dice[i].get() == dice[5]):
-            dice_count[5] += 1
-
-    temp_score += 100 * dice_count[0] #Add 100 for each dice with value 1.
-    temp_score += 50 * dice_count[4] #Add 50 for each dice with value 5.
+            curr_dice_count[5] += 1
+    temp_score += 100 * curr_dice_count[0] #Add 100 for each dice with value 1.
+    temp_score += 50 * curr_dice_count[4] #Add 50 for each dice with value 5.
     straight_count = 0
     pair_count = 0
     #This for loop will count how many pairs or straights are there.
     for i in range(0,6):
-        if dice_count[i] == 1:
+        if curr_dice_count[i] == 1:
             straight_count += 1
-        elif dice_count[i] == 2:
+        elif curr_dice_count[i] == 2:
             pair_count += 1
-        elif dice_count[i] > 2:
+        elif curr_dice_count[i] > 2:
             if i == 0:
-                temp_score += 1000 * (dice_count[i]-2) - (100*dice_count[i])
+                temp_score += 1000 * (curr_dice_count[i]-2) - (100*curr_dice_count[i])
             elif i == 4:
-                temp_score += 5 * 100 * (dice_count[i]-2) - (50*dice_count[i])
+                temp_score += 5 * 100 * (curr_dice_count[i]-2) - (50*curr_dice_count[i])
             else:
-                temp_score += (i+1) * 100 * (dice_count[i]-2)
+                temp_score += (i+1) * 100 * (curr_dice_count[i]-2)
 
     if straight_count == 6:
         temp_score += 350 #Add 350 points if all 6 dice are a straight.
     if pair_count == 3:
         temp_score += 500 #Add 500 points if there are 3 pairs of dice.
-        if dice_count[0] > 0:
-            temp_score -= 100 * dice_count[0] #Subtract 100 for each 1.
-        if dice_count[4] > 0:
-            temp_score -= 50 * dice_count[0] #Subtract 50 for each 5.
+        if curr_dice_count[0] > 0:
+            temp_score -= 100 * curr_dice_count[0] #Subtract 100 for each 1.
+        if curr_dice_count[4] > 0:
+            temp_score -= 50 * curr_dice_count[0] #Subtract 50 for each 5.
     return temp_score
 ##    #Display this message box below only if the player can't score.
 ##    if points.get() == 0:
@@ -121,15 +141,39 @@ def calc_points():
 ##        total.set(new_total)
 
 def reroll():
+    count=0
     for i in range(0, 6):
-        if  curr_dice[i].get() != '':
+        if curr_dice[i].get() != '':#loop to add chosen dice to keep set
             dice_kept[i].set(curr_dice[i].get())
-        if dice_kept[i].get() == '':
+        if dice_kept[i].get() != '':#loop to check for full chosen dice
+            count+=1
+        if dice_kept[i].get() == '':#loop to reset dice
             dice_rolled[i].set(random.choice(dice))
+
+    if count == 6:#if all dice have been kept, then reset curr and kept and rolled just none of the points
+        randomGen()
+
+
+    for i in range(0, 6):#no points then turn endeth and no points received
+        if dice_kept[i].get() == '':
+            curr_dice[i].set(dice_rolled[i].get())
+    pts = points.get()
+    cal = calc_points()#somehow current dice are not being accounted for even while added, check loop above or calc meth
+    if points.get()-calc_points() == 0:
+        calc_points()
+        #Zilch turn
+        points.set(0)
+        #endTurn()
+    else:
+        for i in range(0, 6):
+            if dice_kept[i].get() == '':
+                curr_dice[i].set('')
+
 
 def endTurn():
     new_total = total.get() + points.get()
     total.set(new_total)
+    points.set(0)
 
     # Is the total number of points greater than or equal to 10000?
     # If yes, then display the winning message.
@@ -144,11 +188,12 @@ def reset():
         dice_rolled[i].set("")
         dice_kept[i].set("")
         curr_dice[i].set("")
-        dice_btns[i].config(relief=RAISED)
+        if len(dice_btns) != 0:
+            dice_btns[i].config(relief=RAISED)
     points.set(0)
     total.set(0)
 
-#Forget is like reset, except it only keeps the total amount of points.
+#Forget resets only the dice
 def forget():
     for i in range(0, 6):
         dice_rolled[i].set("")
@@ -156,11 +201,10 @@ def forget():
         curr_dice[i].set("")
         if len(dice_btns) != 0:
             dice_btns[i].config(relief=RAISED)
-    points.set(0)
 
 def newWin1():
     win1 = Toplevel(root)
-    forget()
+    reset()
     root.eval(f'tk::PlaceWindow {str(win1)} center') # center screen
     win1.title("One Player")
     win1.geometry("450x450")
@@ -175,8 +219,6 @@ def newWin1():
         ans = Button(frame, textvariable=dice_rolled[i], height=1, width=2, font=("Helvetica", 20), command=lambda i=i : keep_dice(i))
         ans.pack(side=LEFT)
         dice_btns.append(ans)
-    result_lbls = Label(win1, textvariable=stringme, bg="green", fg="white", font=("Helvetica", 30)).pack()
-
     #TURN OPTIONS
     frame2 = Frame(win1, bg = "green")
     frame2.pack()
@@ -193,7 +235,7 @@ def newWin1():
     reset_btn = Button(win1, text="reset", height=3, width=20, bg="blue", fg="white", command=reset).pack(pady=10)
 
 def newWin2():
-    forget()
+    reset()
     win2 = Toplevel(root)
     root.eval(f'tk::PlaceWindow {str(win2)} center') # center screen
     win2.title("Two Player")
@@ -201,29 +243,27 @@ def newWin2():
     win2.config(bg="green")
     win2.resizable(False, False)
     roll_btn = Button(win2, text="roll", height=3, width=20, bg="blue", fg="white", command=randomGen).pack(pady=10)
-    frame = Frame(win1)
+    frame = Frame(win2)
     frame.pack()
     for i in range(0, 6):
         ans = Button(frame, textvariable=dice_rolled[i], height=1, width=2, font=("Helvetica", 20), command=lambda i=i : keep_dice(i)).pack(side=LEFT)
-    result_lbls = Label(win2, textvariable=stringme, bg="green", fg="white", font=("Helvetica", 30)).pack()
     reset_btn = Button(win2, text="reset", height=3, width=20, bg="blue", fg="white", command=reset).pack(pady=10)
     score_lbl = Label(win2, textvariable=points, bg="green", fg="white", font=("Helvetica", 16)).pack()
     total_lbl = Label(win2, textvariable=total, bg="green", fg="white", font=("Helvetica", 16)).pack()
 
 def newWin3():
     win3 = Toplevel(root)
-    forget()
+    reset()
     root.eval(f'tk::PlaceWindow {str(win3)} center') # center screen
     win3.title("Three Player")
     win3.geometry("450x450")
     win3.config(bg="green")
     win3.resizable(False, False)
     roll_btn = Button(win3, text="roll", height=3, width=20, bg="blue", fg="white", command=randomGen).pack(pady=10)
-    frame = Frame(win1)
+    frame = Frame(win3)
     frame.pack()
     for i in range(0, 6):
         ans = Button(frame, textvariable=dice_rolled[i], height=1, width=2, font=("Helvetica", 20), command=lambda i=i : keep_dice(i)).pack(side=LEFT)
-    result_lbls = Label(win3, textvariable=stringme, bg="green", fg="white", font=("Helvetica", 30)).pack()
     reset_btn = Button(win3, text="reset", height=3, width=20, bg="blue", fg="white", command=reset).pack(pady=10)
     score_lbl = Label(win3, textvariable=points, bg="green", fg="white", font=("Helvetica", 16)).pack()
     total_lbl = Label(win3, textvariable=total, bg="green", fg="white", font=("Helvetica", 16)).pack()
