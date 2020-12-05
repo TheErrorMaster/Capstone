beb548dbd94f527563c5db1a8aa8325b7350ccb4
jttc98 <jttc98@csu.fullerton.edu>
2020-12-04 16:40:11 -0800

    Fleshed out the code
    I added the number of players to play with on a separate window, and I also got rid of the three number of player buttons from the main window as they are redundant. Now this is starting to look like a real game.

--- MainFile/app.py
+++ MainFile/app.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import messagebox #Needed for exit function.
 import random
+import time
 
 #Window variables
 root = Tk()
 root.title('tk::PlaceWindow . center')
 root.title('10,000 Dice')
-root.geometry("400x500")
+root.geometry("400x400")
 root.config(bg="green")
 root.resizable(False, False) #Disallow players from resizing the window.
 dice_btns = []
@@ -15,6 +16,7 @@ points=IntVar() # the current amount of points
 total=IntVar() #the total amount of points
 pointsAI=IntVar()
 totalAI=IntVar()
+turnVar=StringVar()
 
 #See below for the dice unicode:
 #'\u2680' = dice-one
@@ -46,7 +48,7 @@ def randomGen():
     for i in range(0, 6):
         dice_rolled[i].set(random.choice(dice))
 
-    dice_count = [0] * 6
+    #dice_count = [0] * 6
     for i in range(0, 6):
         if (dice_rolled[i].get() == dice[0]):
             dice_count[0] += 1
@@ -191,7 +193,113 @@ def endTurn():
     if (total.get() >= 10000):
         messagebox.showinfo("info", "You won!")
     forget()
+    AITurn()
 
+def AITurn():
+    #initial roll
+    turnVar.set("AI's turn")
+    randomGen()
+
+    root.update()
+    time.sleep(3)
+
+    zero = False
+    while TRUE:
+        #scoring
+        temp_score = 0
+        temp_score += 100 * dice_count[0]  # Add 100 for each dice with value 1.
+        temp_score += 50 * dice_count[4]  # Add 50 for each dice with value 5.
+        straight_count = 0
+        pair_count = 0
+        ls = []
+        # This for loop will count how many pairs or straights are there.
+        for i in range(0, 6):
+            if dice_count[i] == 1:
+                straight_count += 1
+            elif dice_count[i] == 2:
+                pair_count += 1
+            elif dice_count[i] > 2:
+                if i == 0:
+                    temp_score += 1000 * (dice_count[i] - 2) - (100 * dice_count[i])
+                elif i == 4:
+                    temp_score += 5 * 100 * (dice_count[i] - 2) - (50 * dice_count[i])
+                else:
+                    temp_score += (i + 1) * 100 * (dice_count[i] - 2)
+                ls.append(i); #include all triples or more
+
+        if straight_count == 6:
+            temp_score += 350  # Add 350 points if all 6 dice are a straight.
+        if pair_count == 3:
+            temp_score += 500  # Add 500 points if there are 3 pairs of dice.
+            if dice_count[0] > 0:
+                temp_score -= 100 * dice_count[0]  # Subtract 100 for each 1.
+            if dice_count[4] > 0:
+                temp_score -= 50 * dice_count[0]  # Subtract 50 for each 5.
+        if temp_score == 0:
+            zero = True
+        pointsAI.set(pointsAI.get()+temp_score)
+
+        #keeping dice
+        keptcount=0
+        for i in range(0, 6):
+            button = dice_btns[i]
+            if dice_rolled[i].get() == dice[0] or dice_rolled[i].get() == dice[4]:
+                curr_dice[i].set(dice_rolled[i].get())
+                button.config(relief=SUNKEN)
+            if len(ls) == 1 and dice_rolled[i].get() == dice[ls[0]]:
+                curr_dice[i].set(dice_rolled[i].get())
+                button.config(relief=SUNKEN)
+            if len(ls) == 2 and (dice_rolled[i].get() == dice[ls[0]] or dice_rolled[i].get() == dice[ls[1]]):
+                curr_dice[i].set(dice_rolled[i].get())
+                button.config(relief=SUNKEN)
+            if curr_dice[i].get() != "":
+                keptcount+=1;
+            dice_count[i] = 0
+        root.update()
+        time.sleep(3)
+
+        #reroll or endturn
+        if keptcount < 4 and not zero:
+            count = 0
+            # Reroll only the dice that are not chosen yet.
+            for i in range(0, 6):
+                if curr_dice[i].get() != '':  # add chosen dice to keep set
+                    dice_kept[i].set(curr_dice[i].get())
+                if dice_kept[i].get() != '':  # check for full chosen dice
+                    count += 1
+                if dice_kept[i].get() == '':  # reset only the unkept dice
+                    dice_rolled[i].set(random.choice(dice))
+
+            # recalc dice_count
+            for i in range(0, 6):
+                if curr_dice[i].get() == '':
+                    if (dice_rolled[i].get() == dice[0]):
+                        dice_count[0] += 1
+                    if (dice_rolled[i].get() == dice[1]):
+                        dice_count[1] += 1
+                    if (dice_rolled[i].get() == dice[2]):
+                        dice_count[2] += 1
+                    if (dice_rolled[i].get() == dice[3]):
+                        dice_count[3] += 1
+                    if (dice_rolled[i].get() == dice[4]):
+                        dice_count[4] += 1
+                    if (dice_rolled[i].get() == dice[5]):
+                        dice_count[5] += 1
+            # if all dice have been kept, then reset curr and kept,
+            # and rolled to indicate none of the points.
+            if count == 6:
+                curr_pts.set(points.get())  # Keep track of current points.
+                randomGen()
+            root.update()
+            time.sleep(3)
+        else:
+            totalAI.set(totalAI.get()+pointsAI.get())
+            pointsAI.set(0)
+            if (totalAI.get() >= 10000):
+                messagebox.showinfo("info", "You Lost!")
+            forget()
+            turnVar.set("Player's turn")
+            return
         
 #Reset sets all values to default, including the total amount of points.
 def reset():
@@ -209,6 +317,7 @@ def reset():
 #Forget resets only the dice
 def forget():
     for i in range(0, 6):
+        dice_count[i] = 0
         dice_rolled[i].set("")
         dice_kept[i].set("")
         curr_dice[i].set("")
@@ -223,6 +332,8 @@ def newWin1():
     win1.geometry("450x600")
     win1.config(bg="green")
     win1.resizable(False, False)
+    turnVar.set("Player's turn")
+    turn_lbl = Label(win1, textvariable=turnVar, bg="green", fg="white", font=("Helvetica", 16)).pack()
     roll_btn = Button(win1, text="roll", height=3, width=20, bg="blue", fg="white", command=randomGen).pack(pady=10)
 
     #DICE
@@ -235,14 +346,12 @@ def newWin1():
     #TURN OPTIONS
     frame2 = Frame(win1, bg = "green")
     frame2.pack()
-    rollon_btn = Button(frame2, text="Keep points and \nRoll the remaining dice", height=3, width=20, bg="blue", fg="white", command=reroll)
-    rollon_btn.pack(side=LEFT)
-    rollon_btn.pack(padx=10)
-    endturn_btn = Button(frame2, text="End Turn and \nAdd Points to Score", height=3, width=20, bg="blue", fg="white", command=endTurn)
-    endturn_btn.pack(padx=10)
-    endturn_btn.pack(side=LEFT)
+    rollon_btn = Button(frame2, text="Keep points and \nRoll the remaining dice", height=3, width=20, bg="blue", fg="white", command=reroll).pack(side=LEFT, padx=10, pady=10)
+    endturn_btn = Button(frame2, text="End Turn and \nAdd Points to Score", height=3, width=20, bg="blue", fg="white", command=endTurn).pack(padx=10, side=LEFT, pady=10)
 
     #SCOREBOARD
+    frame3 = Frame(win1, bg="green")
+    frame3.pack()
     player_lbl = Label(win1, text="Player's points", bg="green", fg="white", font=("Helvetica", 16)).pack()
     score_lbl = Label(win1, textvariable=points, bg="green", fg="white", font=("Helvetica", 16)).pack()
     player2_lbl = Label(win1, text="Player's total", bg="green", fg="white", font=("Helvetica", 16)).pack()
@@ -295,12 +404,19 @@ def newWin4():
     title = Label(win4, text="How To Play", bg="green", fg="white", font=("Helvetica", 20)).pack()
     how = Label(win4, text="All players start off with the Scoreboard.\n To get onto the Scoreboard, a player must roll at least 1000 points in a single turn.\nEach turn, players start with 6 dice and roll them all.\n From there, players may choose their scoring dice from any points shown on the board.\n After their choice, they may choose to roll the remaining dice OR keep their points (provided they are on the board or the points are above 1000).\n If a player rolls their dice at their start of their turn and there are no points shown, they may re-roll once.\n On any other occasion, if they roll the dice and there are no points shown, that player will forfeit all points and end their turn.\n When a player has scored and chosen all 6 dice, they MUST keep their points for the turn and reroll all dice.\n To win: A player reaching a score at or above 10,000 becomes the temporary winner, then they must keep their lead as all other players get a chance to roll one more turn.\n The highest score after all players have rolled is the Official Winner", bg="green", fg="white", bd=2, relief="solid", font=("Helvetica", 12)).pack()
 
+def select():
+    win5 = Toplevel(root)
+    win5.title("Players")
+    win5.geometry("400x400")
+    win5.config(bg="green")
+    title = Label(win5, text="Select the number of playes", bg="green", fg="white", font=("Helvetica", 20)).pack()
+    user1_btn = Button(win5, text="one", height=3, width=20, bg="blue", fg="white", command=newWin1).pack(pady=10)
+    user2_btn = Button(win5, text="two", height=3, width=20, bg="blue", fg="white", command=newWin2).pack(pady=10)
+    user3_btn = Button(win5, text="three", height=3, width=20, bg="blue", fg="white", command=newWin3).pack(pady=10)
+
 # Varaiables with location
 title_lbl = Label(root, text="10,000 Dice", fg="white", bg="green", font=("Helvetica", 16)).pack()
-player_lbl = Label(root, text="Select the Number of Players", fg="white", bg="green", font=("Helvetica", 16)).pack()
-user1_btn = Button(root, text="one", height=3, width=20, bg="blue", fg="white", command=newWin1).pack(pady=10)
-user2_btn = Button(root, text="two", height=3, width=20, bg="blue", fg="white", command=newWin2).pack(pady=10)
-user3_btn = Button(root, text="three", height=3, width=20, bg="blue", fg="white", command=newWin3).pack(pady=10)
+play_btn = Button(root, text="play", height=3, width=20, bg="blue", fg="white", command=select).pack(pady=10)
 help_btn = Button(root, text="help", height=3, width=20, bg="blue", fg="white", command=newWin4).pack(pady=10)
 quit_btn = Button(root, text="quit", height=3, width=20, bg="blue", fg="white", command=on_closing).pack(pady=10)
 
